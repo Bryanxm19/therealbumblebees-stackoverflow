5145956c335dccdb2eed509e1e013b5eed802bd8
Adriel Saporta <email": null>
2017-04-27 16:27:11 -0400

    User can sign up, login, and log out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,19 @@
 class User < ActiveRecord::Base
   validates :username, :email, presence: true, uniqueness: true
 
-  has_secure_password
-
   has_many :questions
   has_many :answers
   has_many :votes
+
+  has_secure_password
+
+  # def self.authenticate(given_email, given_password)
+  #   user = User.find_by(email: given_email)
+  #   binding.pry
+  #   if user != nil && user.password == given_password
+  #     return user
+  #   else
+  #     return nil
+  #   end
+  # end
 end
