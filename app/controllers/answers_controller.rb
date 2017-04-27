5e6985901dee8bcb22c20bc5354c010ddc8b3ab2
Alex Camacho & Bryan Mateer & Thomas Feng <pair+alex-e-camacho.bryanxm19.realtomfeng@devbootcamp.com>
2017-04-27 17:48:56 -0400

    Add routes for editing and deleting answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,1 +1,25 @@
+get '/answers/:id/edit' do
+  @answer = Answer.find(params[:id])
+  erb :'/answers/edit'
+end
 
+put '/answers/:id' do
+  @answer = Answer.find(params[:id])
+  @answer.assign_attributes(params[:answer])
+
+  if @answer.save
+    redirect "/questions/#{@answer.question_id}"
+  else
+    @errors = @answer.errors.full_messages
+    erb :'/answers/edit'
+  end
+end
+
+delete '/answers/:id' do
+  @answer = Answer.find(params[:id])
+
+  if logged_in? && (current_user.id == @answer.user_id)
+    @answer.destroy
+  end
+  redirect "/questions/#{@answer.question_id}"
+end
