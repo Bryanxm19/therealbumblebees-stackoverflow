f430250b6d1453c25da7def33e20080bc114902c
Thomas Feng <feng.thom@gmail.com>
2017-04-27 16:31:57 -0400

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,4 @@ class User < ActiveRecord::Base
   has_many :votes
 
   has_secure_password
-
-  # def self.authenticate(given_email, given_password)
-  #   user = User.find_by(email: given_email)
-  #   binding.pry
-  #   if user != nil && user.password == given_password
-  #     return user
-  #   else
-  #     return nil
-  #   end
-  # end
 end
