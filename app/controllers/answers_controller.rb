94b61bb89181e98e244118ab49d85c31a3b89605
Thomas Feng <email": null>
2017-04-28 11:58:35 -0400

    added edit and delete routes for questions and answers, protected more routes

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,17 +1,26 @@
 get '/answers/:id/edit' do
   @answer = Answer.find(params[:id])
+  if logged_in? && (current_user.id == @answer.user_id)
+  @answer = Answer.find(params[:id])
   erb :'/answers/edit'
+  else
+  redirect "/questions/#{@answer.question_id}"
+  end
+
 end
 
 put '/answers/:id' do
   @answer = Answer.find(params[:id])
   @answer.assign_attributes(params[:answer])
-
-  if @answer.save
-    redirect "/questions/#{@answer.question_id}"
+  if logged_in? && (current_user.id == @answer.user_id)
+    if @answer.save
+      redirect "/questions/#{@answer.question_id}"
+    else
+      @errors = @answer.errors.full_messages
+      erb :'/answers/edit'
+    end
   else
-    @errors = @answer.errors.full_messages
-    erb :'/answers/edit'
+  redirect "/questions/#{@answer.question_id}"
   end
 end
 
@@ -19,7 +28,6 @@ end
 
 delete '/answers/:id' do
   @answer = Answer.find(params[:id])
-
   if logged_in? && (current_user.id == @answer.user_id)
     @answer.destroy
   end
