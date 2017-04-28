bbcaf53edbe2df5511c315023e2d4c8c98016d92
Adriel Saporta <email": null>
2017-04-28 10:47:12 -0400

    Allow user to vote for an answer

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $("button.up-down-vote-button").on("click", function(event) {
+  $("button.up-button").on("click", function(event) {
     event.preventDefault();
     var button = this
 
@@ -13,9 +13,27 @@ $(document).ready(function() {
       data: {question_id: question_id, answer_id: answer_id, vote_type: vote_type}
     })
     .done(function(response) {
-      console.log(response)
       $(button).next().next().html(response)
-      // change color of up arrow
+      $(button).css("color", "gold")
+    })
+  });
+
+  $("button.down-button").on("click", function(event) {
+    event.preventDefault();
+    var button = this
+
+    var question_id = $(this).parent().attr("question")
+    var answer_id = $(this).parent().attr("answer")
+    var vote_type = $(this).attr("name")
+
+    $.ajax({
+      url: "/questions/" + question_id + "/answers/" + answer_id + "/vote",
+      method: "POST",
+      data: {question_id: question_id, answer_id: answer_id, vote_type: vote_type}
+    })
+    .done(function(response) {
+      $(button).prev().prev().html(response)
+      $(button).css("color", "DarkRed")
     })
   })
 });
