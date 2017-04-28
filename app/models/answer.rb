e169b0cfa29c6f0df2b39c3da43ea36caa3f2cbb
Thomas Feng <email": null>
2017-04-28 12:23:18 -0400

    added polymorphic models and migrations for comments

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -5,6 +5,7 @@ class Answer < ActiveRecord::Base
   belongs_to :user
   belongs_to :question
   has_many :votes, as: :votable
+  has_many :comments, as: :commentable
 
   include Countable
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -4,6 +4,7 @@ class Question < ActiveRecord::Base
   belongs_to :user
   has_many :answers
   has_many :votes, as: :votable
+  has_many :comments, as: :commentable
 
   include Countable
 
