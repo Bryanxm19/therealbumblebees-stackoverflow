d80a7adc054eb9bb1883218f190256ecd0d9d81f
Adriel Saporta <email": null>
2017-04-28 10:37:58 -0400

    Fix question show view to submit user_id when user submits answer

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,13 +3,13 @@
 end
 
 50.times do
-  Question.create(title: (Faker::Hipster.sentence(2).chop + "?"), body: (Faker::Hipster.sentence(5).chop + "?"), user_id: rand(50))
+  Question.create(title: (Faker::Hipster.sentence(2).chop + "?"), body: (Faker::Hipster.sentence(5).chop + "?"), user_id: (rand(49)+1))
 end
 
 100.times do
-  Answer.create(text: Faker::Hacker.say_something_smart, user_id: rand(50), question_id: rand(50))
+  Answer.create(text: Faker::Hacker.say_something_smart, user_id: (rand(49)+1), question_id: (rand(49)+1))
 end
 
 500.times do
-  Vote.create(user_id: rand(50), votable_id: rand(50), votable_type: ["Question", "Answer"].sample, up_down: ["up", "down"].sample)
+  Vote.create(user_id: (rand(49)+1), votable_id: (rand(49)+1), votable_type: ["Question", "Answer"].sample, up_down: ["up", "down"].sample)
 end
