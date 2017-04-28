4bf9fb13d2225d24b1272c1ee47e0128a4d1cb85
Adriel Saporta <email": null>
2017-04-28 12:17:57 -0400

    When user clicks down button twice it changes color and deletes registered vote

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,7 @@
 var clicks = true;
 
 $(document).ready(function() {
+  // Upvote Button
   $("button.up-button").on("click", function(event) {
     event.preventDefault();
     var button = this
@@ -34,9 +35,9 @@ $(document).ready(function() {
       })
       clicks = true;
     }
-
   });
 
+  // Downvote Button
   $("button.down-button").on("click", function(event) {
     event.preventDefault();
     var button = this
@@ -45,14 +46,30 @@ $(document).ready(function() {
     var answer_id = $(this).parent().attr("answer")
     var vote_type = $(this).attr("name")
 
-    $.ajax({
-      url: "/questions/" + question_id + "/answers/" + answer_id + "/vote",
-      method: "POST",
-      data: {question_id: question_id, answer_id: answer_id, vote_type: vote_type}
-    })
-    .done(function(response) {
-      $(button).prev().prev().html(response)
-      $(button).css("color", "DarkRed")
-    })
-  })
+    if (clicks) {
+      // odd clicks
+      $.ajax({
+        url: "/questions/" + question_id + "/answers/" + answer_id + "/vote",
+        method: "POST",
+        data: {question_id: question_id, answer_id: answer_id, vote_type: vote_type}
+      })
+      .done(function(response) {
+        $(button).prev().prev().html(response)
+        $(button).css("color", "darkred")
+      })
+      clicks = false;
+    } else {
+      // even clicks
+      $.ajax({
+        url: "/questions/" + question_id + "/answers/" + answer_id + "/unvote",
+        method: "POST",
+        data: {question_id: question_id, answer_id: answer_id}
+      })
+      .done(function(response) {
+        $(button).prev().prev().html(response)
+        $(button).css("color", "dimgrey")
+      })
+      clicks = true;
+    }
+  });
 });
